915c99a3785d5758f0bfc4514a4585993c4c7891
gregrutnam <111754256+gregrutnam@users.noreply.github.com>
2022-12-16 13:34:45 +0100

    weather type made

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -10,9 +10,39 @@ export type cityInfoObj = {
   
 }
 
+export type weatherObj = {
+  base: string
+  clouds: {all: number}
+  cod: number
+  coord: {lon: number, lat: number}
+  dt: number
+  id: number
+  main: {feels_like: number, grnd_level?: number | undefined, humidity: number, pressure: number, sea_level?: number | undefined, temp: number, temp_max: number, temp_min: number}
+  name: string
+  sys: {country?: string | undefined, sunrise: number, sunset: number}
+  timezone: number
+  visibility: number
+  weather: [{id: number, main: string, description: string, icon: string}]
+  wind: {deg: number, gust: number, speed: number}
+}
+
 function App() {
   const [cityInfo, setCityInfo] = useState <cityInfoObj> ({lat: 0, lon: 0, city: ""});
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState <weatherObj> ({
+    base: "",
+    clouds: {all: 0},
+    cod: 0,
+    coord: {lon: 0, lat: 0},
+    dt: 0,
+    id: 0,
+    main: {feels_like: 0, grnd_level: 0, humidity: 0, pressure: 0, sea_level: 0, temp: 0, temp_max: 0, temp_min: 0},
+    name: "",
+    sys: {country: "", sunrise: 0, sunset: 0},
+    timezone: 0,
+    visibility: 0,
+    weather: [{id: 0, main: "", description: "", icon: ""}],
+    wind: {deg: 0, gust: 0, speed: 0}
+  });
   const [userInput, setUserInput] = useState("");
   const [search, setSearch] = useState("");
   const apiKey = "5719296ca5bace34a0a2a4c27f534df4"
@@ -62,7 +92,7 @@ function App() {
   return (
     <div className="App">
       <Search handleUserInput={handleUserInput} handleClick={handleClick} />
-      <Display cityInfo={cityInfo}/>
+      <Display cityInfo={cityInfo} weather={weather}/>
     </div>
   );
 }
--- src/Components/Display/index.tsx
+++ src/Components/Display/index.tsx
@@ -1,12 +1,17 @@
  import { cityInfoObj } from "../App/App"
 type PropsObj = {
-    cityInfo: cityInfoObj
+    cityInfo: cityInfoObj,
+    weather: {}
 }
 
 
 export default function Display(props: PropsObj ){
     const {lat, lon, city} = props.cityInfo
+    const weather = props.weather
+
     return <div>
         <h2>{city}</h2>
+        <h3>{weather.main.temp}</h3>
+        
     </div>
 }
